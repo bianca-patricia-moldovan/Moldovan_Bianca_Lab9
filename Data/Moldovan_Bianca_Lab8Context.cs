93c03605c07e0adcbf4d12976da7f8b27a3db81b
Bianca Patricia Moldovan <biahappyday@yahoo.com>
2020-12-01 21:34:47 +0200

    Add project files.

---
+++ Data/Moldovan_Bianca_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moldovan_Bianca_Lab8.Models;
+
+namespace Moldovan_Bianca_Lab8.Data
+{
+    public class Moldovan_Bianca_Lab8Context : DbContext
+    {
+        public Moldovan_Bianca_Lab8Context (DbContextOptions<Moldovan_Bianca_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Moldovan_Bianca_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Moldovan_Bianca_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Moldovan_Bianca_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Moldovan_Bianca_Lab8.Data;
+using Moldovan_Bianca_Lab8.Models;
+
+namespace Moldovan_Bianca_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Moldovan_Bianca_Lab8.Data.Moldovan_Bianca_Lab8Context _context;
+
+        public IndexModel(Moldovan_Bianca_Lab8.Data.Moldovan_Bianca_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.ToListAsync();
+        }
+    }
+}
